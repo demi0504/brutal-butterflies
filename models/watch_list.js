efb9fb6da1c2565072d41d2f6f8afb69ecbbdde4
demi0504 <ripley.demi@gmail.com>
2020-08-13 20:56:09 -0500

    fixed lint error

--- models/watch_list.js
+++ models/watch_list.js
@@ -20,7 +20,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING(255),
       allowNull: false
     },
-    rotten_tom: {
+    rottenTom: {
       type: DataTypes.STRING(100),
       allowNull: false
     },
