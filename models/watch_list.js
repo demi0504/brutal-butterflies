9f795b83732bb9a78c0239ee3de795cbca077cfb
demi0504 <ripley.demi@gmail.com>
2020-08-13 20:55:09 -0500

    model for watched movies

---
+++ models/watch_list.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, DataTypes) {
+  const Watch_list = sequelize.define("Watch_list", {
+    title: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    plot: {
+      type: DataTypes.STRING(1000),
+      allowNull: false
+    },
+    poster: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    plot: {
+      type: DataTypes.STRING(1000),
+      allowNull: false
+    },
+    trailer: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    rotten_tom: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    watched: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    favorite: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  });
+};
