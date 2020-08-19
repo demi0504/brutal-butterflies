6d54dddca75994de7339ac319262a1418c2e5140
Jordan Long <jblong27@gmail.com>
2020-08-18 20:58:56 -0500

    adding ajax request and code to hide api key

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -1,6 +1,23 @@
+const searchBtn = $("#search-btn");
+const results = $("#movieSearch");
+
+const dotenv = require("dotenv").config();
+const apiKey = process.env.API_KEY;
+// require dotenv to hide our api key
+
+
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
+
+  // ajax call to return the movie info
+  $.ajax({
+    url: `http://www.omdbapi.com/?apikey=${apiKey}&t` + results.trim();,
+    methos: "GET"
+  }).then(response => {
+    console.log(response);
+  });
+
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
