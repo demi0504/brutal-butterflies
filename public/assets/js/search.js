ef967f0b8b0505ee83d529a9be2b61788e37a6be
Jordan Long <jblong27@gmail.com>
2020-08-18 21:44:48 -0500

    update search.js and members.js

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,9 +1,23 @@
+const searchBtn = $("#search-btn");
+const userInput = $("#movieSearch");
+
+const dotenv = require("dotenv").config();
+const apiKey = process.env.API_KEY;
+// require dotenv to hide our api key
+
 $(document).ready(() => {
   const addToWatchList = (movie) => {
     $.post("/user/3/watchlist", movie).then(result => {
       console.log("api result", result);
     });
   };
+  // ajax call to return the movie info
+  $.ajax({
+    url: `http://www.omdbapi.com/?apikey=${apiKey}&t` + userInput.trim(),
+    methos: "GET"
+  }).then(response => {
+    console.log(response);
+  });
 
   const resultsList = $("#searchResults");
   mockResults.results.forEach(result => {
@@ -20,17 +34,17 @@ $(document).ready(() => {
   });
 
   // Click handler to watchlist button
-  $(".add-to-watchlist").click(event => {
-    event.preventDefault();
-    const target = event.target;
-    // create movie object
-    const movieContainer = $(target).parents(".movie-result");
-    const movieTitle = movieContainer.find(".title").text();
-    const movieObj = {
-      title: movieTitle
-    };
-    addToWatchList(movieObj);
-  });
+    $(".add-to-watchlist").click(event => {
+      event.preventDefault();
+      const target = event.target;
+      // create movie object
+      const movieContainer = $(target).parents(".movie-result");
+      const movieTitle = movieContainer.find(".title").text();
+      const movieObj = {
+        title: movieTitle
+      };
+      addToWatchList(movieObj);
+    });
 
   // post request to correct route
 });
