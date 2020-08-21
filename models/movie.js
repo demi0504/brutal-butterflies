2ecc3f3b9817a03b30aa1687b4101cd230c00ba4
demi0504 <ripley.demi@gmail.com>
2020-08-20 19:13:37 -0500

    html route for watchlist

--- models/movie.js
+++ models/movie.js
@@ -13,23 +13,23 @@ module.exports = function(sequelize, DataTypes) {
     },
     plot: {
       type: DataTypes.STRING(1000),
-      allowNull: false
+      allowNull: true
     },
     genre: {
       type: DataTypes.STRING(30),
-      allowNull: false
+      allowNull: true
     },
     length: {
       type: DataTypes.STRING(30),
-      allowNull: false
+      allowNull: true
     },
     actors: {
       type: DataTypes.STRING(1000),
-      allowNull: false
+      allowNull: true
     },
     poster: {
       type: DataTypes.TEXT,
-      allowNull: false
+      allowNull: true
     },
     trailer: {
       type: DataTypes.STRING(255),
@@ -37,7 +37,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     rottenTom: {
       type: DataTypes.STRING(100),
-      allowNull: false
+      allowNull: true
     },
     createdAt: {
       type: DataTypes.DATE,
